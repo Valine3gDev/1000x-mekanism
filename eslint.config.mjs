aee2a30e707f2e25b684fb0cefbe6ef41d9ed0c8
Meatwo310 <72017364+Meatwo310@users.noreply.github.com>
2025-04-11 23:04:35 +0900

    ESLintとESLint Stylisticを導入

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import js from "@eslint/js"
+import stylistic from "@stylistic/eslint-plugin"
+import { defineConfig } from "eslint/config"
+import globals from "globals"
+
+export default defineConfig([
+    stylistic.configs.customize({
+        indent: 4,
+        semi: false,
+        quotes: "double",
+    }),
+    {
+        files: ["**/*.{js,mjs,cjs}"],
+        plugins: {
+            js,
+            "@stylistic": stylistic,
+        },
+        languageOptions: {
+            globals: globals.node,
+        },
+        extends: ["js/recommended"],
+        rules: {
+            // Possible Problems
+            "no-undef": "off",
+            "no-irregular-whitespace": "off",
+            "no-unused-vars": ["warn", { args: "none" }],
+            "no-loss-of-precision": "warn",
+
+            // Suggestions
+            "no-sequences": "error",
+            "prefer-const": "error",
+            "no-var": "error",
+
+            // Stylistic rules
+            "@stylistic/array-bracket-newline": "error",
+        },
+    },
+])
